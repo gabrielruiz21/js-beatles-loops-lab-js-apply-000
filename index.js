435672afc76cad31b897e135fd3be2c9750da7da
GABRIEL ESTEBAN RUIZ MENESES <gabriel_ruiz21@hotmail.com>
2018-05-20 00:56:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(x, y){
+  var emptyArray = [];
+  
+  for(var i = 0; i < x.length; i++)
+  {
+    var push = x[i] + " plays " +  y[i]
+    emptyArray.push(push)
+  }
+  
+  return emptyArray
+}
+
+function johnLennonFacts(x){
+  var y = []
+  var counter = 0
+  var index = x.length
+  while (counter < index) {
+    y.push(x[counter] + "!!!")
+    counter ++
+  }
+  return y; 
+}
+
